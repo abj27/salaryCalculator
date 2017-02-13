3169e6e41d1940e4990256ddca73fcb82d23acaa
Alberto Espinal <alberto.espinal.c@gmail.com>
2017-02-13 11:48:46 +0000

    refactoring in the test domain

--- src/salaryCalculatorService.js
+++ src/salaryCalculatorService.js
@@ -8,16 +8,16 @@
 */
 var employeeTypes = {
     fixedSalary : "fixedSalary",
-    fixedSalaryWithOvertime: "fixedSalaryWithOvertime",
+    regular: "regular",
     developer: "developer",
     contractor: "contractor"
 };
 
-var Employee = function(type, name, pricePerHour){
+var Employee = function(type, name, hourlyRate){
    this.id = Math.random();
    this.type = type;
    this.name = name;
-   this.pricePerHour = pricePerHour;
+   this.hourlyRate = hourlyRate;
 };
 var employees = [];
 
@@ -31,23 +31,88 @@ var PicaPolloAPi= function(){
        return 140;
     }
 }
+
+
+var WorkedHoursSevice = function(dailyWorkSheets){
+    this.workingHoursInADay = 8;
+    let isInsideWeek = function(firstDayOfWeek, date){
+        return date >= firstDayOfWeek && date <= firstDayOfWeek.setDate(firstDayOfWeek.getDate() + 6);
+    }
+    this.getOverworkSheet = (firstDayOfWeek, employeeId) =>{
+         //Calling public function inside another one of the same class
+         return this.getWorkSheet(firstDayOfWeek, employeeId)
+                    .map(x => x >= this.workingHoursInADay? x - this.workingHoursInADay : 0)
+    };
+    this.getWorkSheet = (firstDayOfWeek, employeeId) =>{
+        dailyWorkSheets
+             .find(x => x.employeeId == employeeId && this.isInsideWeek(firstDayOfWeek, x.date))
+    };
+};
+var workedHoursService = new WorkedHoursSevice(dailyWorkSheets);
+
+
+var BaseSalaryCalculator = function(){
+    this.workingHoursInWeek= 44;
+    this.calculate = (employeeType, hourlyRate) =>{
+       switch (employeeType) {
+           case employeeType.fixedSalary:
+           case employeeType.regular:
+           case employeeType.developer:
+               return hourlyRate * this.workingHoursInWeek
+           case employeeType.contractor:
+               return 0;
+       }
+    }
+};
+var baseSalaryCalculator = new BaseSalaryCalculator();
+
+var OvertimeCalculator = function(workedHoursService, picapolloAPi){
+
+    this.overworkRate = 1.5;
+    this.calculate = (employeeId,employeeType, hourlyRate, firstDayOfWeek) =>{
+       switch (employeeType) {
+           case employeeType.fixedSalary:
+               return 0
+           case employeeType.regular:
+              var overworkedHours =workedHoursService
+              .getOverworkSheet(firstDayOfWeek, employeeId)
+              .reduce((accumulator, current) => accumulator + current);
+            return overworkedHours * hourlyRate * this.overworkRate;
+           case employeeType.developer:
+               var overworkedDays= workedHoursService
+               .getOverworkSheet(firstDayOfWeek, employeeId).filter(x => x >0);
+               return overworkedDays *picapolloAPi.getPicapolloPrice();
+           case employeeType.contractor:
+               var workedHours = workedHoursService
+               .getWorkSheet(firstDayOfWeek, employeeId)
+               .reduce((accumulator, current) => accumulator + current);
+               return workedHours * hourlyRate;
+       }
+    }
+};
+var overtimeCalculator = new OvertimeCalculator(workedHoursService,new PicaPolloAPi());
+
 var dailyWorkSheets = [];
 
-var paymentCalculator= {
-    getPayment: function(employee, firstDayOfWeek){
-       if(employee.type === employeeTypes.fixedSalary) {
-           return employee.pricePerHour * 44;
+var PaymentCalculator= function(baseSalaryCalculator, overtimeCalculator){
+   this. getPayment = function(employee, firstDayOfWeek){
+       var baseSalary =baseSalaryCalculator.calculate(employee.type, employee.hourlyRate);
+       var overtime = overtimeCalculator.calculate(employee.id, employee.type, employee.hourlyRate, firstDayOfWeek);
+       return baseSalary + overtime;
+       /*
+      if(employee.type === employeeTypes.fixedSalary) {
+           return employee.hourlyRate * 44;
        }
-       else if(employee.type === employeeTypes.fixedSalaryWithOvertime){
+       else if(employee.type === employeeTypes.regular){
            let hoursWorkedInAWeek = dailyWorkSheets
              .find(x => x.employeeId == employee.id && x.date >= firstDayOfWeek && x.date <= firstDayOfWeek.setDate(firstDayOfWeek.getDate() + 6))
              .map(x => x.hoursWorked)
              .reduce((accumulator, current) =>{ return accumulator + current});
            if(hoursWorkedInAWeek > 44) {
-              return employee.pricePerHour * 44 + (hoursWorkedInAWeek - 44) * 1.5
+              return employee.hourlyRate * 44 + (hoursWorkedInAWeek - 44) * 1.5
            }
            else{
-               return employee.pricePerHour * 44;
+               return employee.hourlyRate * 44;
            }
 
        }
@@ -56,7 +121,7 @@ var paymentCalculator= {
              .find(x => x.employeeId == employee.id && x.date >= firstDayOfWeek && x.date <= firstDayOfWeek.setDate(firstDayOfWeek.getDate() + 6))
              .map(x => x.hoursWorked)
              .reduce((accumulator, current) =>{ return accumulator + current});
-          return hoursWorkedInAWeek * employee.pricePerHour;
+          return hoursWorkedInAWeek * employee.hourlyRate;
        }
        else if(employee.type === employeeTypes.developer){
            let extraPay= dailyWorkSheets
@@ -65,16 +130,25 @@ var paymentCalculator= {
              .reduce((accumulator, current) =>{ if(current.hoursWorked > 8){
                  return accumulator + PicaPolloAPi().getPicapolloPrice(current.date);
              }}, 0);
-          return 44 * employee.pricePerHour + extraPay;
+          return 44 * employee.hourlyRate + extraPay;
        }
+      */
     }
 };
+var paymentCalculator = new PaymentCalculator(baseSalaryCalculator, overtimeCalculator);
+
 module.exports ={
-    employees : employees,
     Employee : Employee,
+    employees : employees,
+    employeeTypes: employeeTypes,
     DailyWorkSheet: DailyWorkSheet,
     dailyWorkSheets: dailyWorkSheets,
-    dailyWorkSheets: dailyWorkSheets,
+    PaymentCalculator: PaymentCalculator,
     paymentCalculator: paymentCalculator,
-    employeeTypes: employeeTypes
+    BaseSalaryCalculator: BaseSalaryCalculator,
+    baseSalaryCalculator: baseSalaryCalculator,
+    OvertimeCalculator: OvertimeCalculator,
+    overtimeCalculator: overtimeCalculator,
+    WorkedHoursService: WorkedHoursSevice,
+    workedHoursService: workedHoursService
 }
--- src/salaryCalculatorTests.js
+++ src/salaryCalculatorTests.js
@@ -1,35 +1,38 @@
-// const td = require("testdouble");
+const td = require("testdouble");
 const faker = require("faker");
 import {expect} from "chai";
-const paymentCalculator = require("./salaryCalcualatorService").paymentCalculator;
-const Employee= require("./salaryCalcualatorService").Employee;
-const employeeTypes= require("./salaryCalcualatorService").employeeTypes;
-const dailyWorkSheets= require("./salaryCalcualatorService").dailyWorkSheets;
-const DailyWorkSheet= require("./DailyWorkSheet").DailyWorkSheet;
+const PaymentCalculator = require("./salaryCalculatorService").PaymentCalculator;
+const Employee= require("./salaryCalculatorService").Employee;
+const employeeTypes= require("./salaryCalculatorService").employeeTypes;
 describe("Payment calculator get payment", function () {
   //System Under Test
   let sut;
   let employee;
-  let pricePerHour;
-  let workingHoursInWeek = 44;
-  let overTimeFactor = 1.5;
+  let paymentCalculator;
+  let overtimeCalculator;
+  let baseSalaryCalculator;
+//   let pricePerHour;
+//   let workingHoursInWeek = 44;
+//   let overTimeFactor = 1.5;
   let firstDayOfWeek =new Date();
   beforeEach(function () {
     //Arrange
-    sut = paymentCalculator.getPayment;
-    pricePerHour = 7;
-    while(dailyWorkSheets.length){
-        dailyWorkSheets.pop();
+    employee = new Employee(employeeTypes.fixedSalary, faker.lorem.words, 5);
+    baseSalaryCalculator = {
+        calculate: td.func()
+    };
+    overtimeCalculator = {
+        calculate: td.func()
     }
+    paymentCalculator = new PaymentCalculator(baseSalaryCalculator, overtimeCalculator);
+    sut = paymentCalculator.getPayment.bind(paymentCalculator);
   });
-  var setWeekHours = function(initialDate, hours){
-    var date = new Date(initialDate.getTime())
-    dailyWorkSheets.push(new DailyWorkSheet(employee.id, date, hours[0]));
-    for(var i= 1 ; i< hours.length; i++){
-      dailyWorkSheets.push(new DailyWorkSheet(employee.id, date.setDate(date.getDay() +1), hours[i] ));
-    }
-  };
-
+  it("should return the sum of the base salary and the overtime", function(){
+      td.when(overtimeCalculator.calculate(employee)).thenReturn(5);
+      td.when(baseSalaryCalculator.calculate(employee)).thenReturn(4);
+      expect(sut(employee, firstDayOfWeek)).to.equal(9);
+  });
+/*
   it("should return a full week salary for a fixed salary employee", function () {
     //Act
     employee = new Employee(employeeTypes.fixedSalary, faker.lorem.word(), pricePerHour );
@@ -69,5 +72,112 @@ describe("Payment calculator get payment", function () {
     });
     expect(sut(employee, firstDayOfWeek)).to.equal(workedHours * pricePerHour);
   });
+*/
+});
+
+
+const WorkedHoursService= require("./salaryCalculatorService").WorkedHoursService;
+const DailyWorkSheet= require("./salaryCalculatorService").DailyWorkSheet;
+describe("Worked Hours service, getting the work sheet", function () {
+  let sut;
+  let dailyWorkSheets =[];
+  let employee = new Employee(employeeTypes.fixedSalary, faker.lorem.words, 5);
+  let initialDate = new Date();
+  let expectedHours = [8,7,8,8,8,2];
+  beforeEach(function () {
+    context ={
+        workedHoursInADay : 8,
+    };
+    var workedHoursService = new WorkedHoursService(dailyWorkSheets);
+    sut = workedHoursService.getWorkSheet.bind(workedHoursService);
+  });
+
+
+  it("Should return a list with the hours worked on that week", function(){
+     setWeekHours(initialDate, expectedHours)
+     var results = sut(initialDate, employee.id);
+     expect(results.length).to.equal(expectedHours.length);
+     for(var i =0; i< results.length ; i++){
+         expect(results[i]).to.equal(expectedHours[i])
+     }
+  });
+
+  var setWeekHours = function(initialDate, hours){
+    var date = new Date(initialDate.getTime())
+    dailyWorkSheets.push(new DailyWorkSheet(employee.id, date, hours[0]));
+    for(var i= 1 ; i< hours.length; i++){
+      dailyWorkSheets.push(new DailyWorkSheet(employee.id, date.setDate(date.getDay() +1), hours[i] ));
+    }
+  };
 
 });
+
+describe("Worked Hours service, getting the overwork sheet", function () {
+  let sut;
+  let dailyWorkSheets =[];
+  let employee = new Employee(employeeTypes.fixedSalary, faker.lorem.words, 5);
+  let initialDate = new Date();
+  let expectedWorkedHours= [8,7,9,8,10,2];
+  let expectedOverworkedHours= [0,0,1,0,2,0];
+  let context;
+  beforeEach(function () {
+    // the context trick in javascript :D
+    context ={
+        workedHoursInADay : 8,
+        getWorkSheet:td.func()
+    };
+    var workedHoursService = new WorkedHoursService(dailyWorkSheets);
+    sut = workedHoursService.getOverworkSheet.bind(context);
+  });
+  it("Should return a list with the over worked hours per day on that week", function(){
+     td.when(context.getWorkSheet(initialDate, employee.id)).thenReturn(expectedWorkedHours);
+     var results = sut(initialDate, employee.id);
+     expect(results.length).to.equal(expectedOverworkedHours.length);
+     for(var i =0; i< results.length ; i++){
+         expect(results[i]).to.equal(expectedOverworkedHours[i])
+     }
+  });
+});
+
+
+describe("Base salary calculator, calculate", function () {
+  let sut;
+  let dailyWorkSheets =[];
+
+  let initialDate = new Date();
+  let workingHoursInWeek = 44;
+  let context;
+  let pricePerHour =8;
+  beforeEach(function () {
+    // the context trick in javascript :D
+    context ={
+        workedHoursInADay : 8,
+        getWorkSheet:td.func()
+    };
+    var workedHoursService = new WorkedHoursService(dailyWorkSheets);
+    sut = workedHoursService.getOverworkSheet.bind(context);
+  });
+  var getEmployee = function(type){
+    return new Employee(type, faker.lorem.word(),pricePerHour);
+  }
+  it("should return a full week salary for a fixed salary employee", function () {
+    let employee = getEmployee(employeeTypes.fixedSalary);
+    expect(sut(employee, initialDate)).to.equal(workingHoursInWeek * pricePerHour);
+  });
+
+  it("should return a full week salary for a regular employee", function () {
+    let employee = getEmployee(employeeTypes.regular);
+    expect(sut(employee, initialDate)).to.equal(workingHoursInWeek * pricePerHour);
+  });
+
+  it("should return a full week salary for a regular developer", function () {
+    let employee = getEmployee(employeeTypes.developer);
+    expect(sut(employee, initialDate)).to.equal(workingHoursInWeek * pricePerHour);
+  });
+
+  it("should return 0 for a contractor", function () {
+    let employee = getEmployee(employeeTypes.contractor);
+    expect(sut(employee, initialDate)).to.equal(0);
+  });
+});
+
